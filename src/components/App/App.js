73923e5ccfc667a3547fa27ab1812b2608a5a436
Mimi Starnes <mstarnes@charter.net>
2023-07-17 22:08:16 -0400

    Added weather banner

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,22 @@
 // import logo from './';
 import "./App.css";
-// import Header from "../Header/Header";
+import Header from "../Header/Header";
 
 function App() {
   return (
     <div>
-      <header className="header">
-        <div className='header__logo'>
-        <div>
-          <img src='/images/logo.svg' alt='logo'/>
-        </div>
-        <div>Date</div>
-        </div>
-        <div className='header__avatar-logo'>
-        <div>
-          <button type="text">Add New Clothes</button> 
-        </div>
-        <div>Name</div>
-        <div>
-        <img src='/images/Avatar.svg' alt='Avatar'/>
-        </div>
-        </div>
-      </header>
+      <Header />
+      <main className="main">
+        <section className="weather" id="weather">
+          <div className="weather__info">75</div>
+          <img
+            src="./images/day/sunny.svg"
+            alt="sunny"
+            className="weather__image"
+          ></img>
+        </section>
+        <section id="card-section">Card Section</section>
+      </main>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,27 @@
-// return (
-//     <div>
-//       <header className="header">
-//         <div className='header__logo'>
-//         <div>Logo</div>
-//         <div>Date</div>
-//         </div>
-//         <div className='header__avatar-logo'>
-//         <div>
-//           <button type="text">Add New Clothes</button> 
-//         </div>
-//         <div>Name</div>
-//         <div>Avatar Logo</div>
-//         </div>
-//       </header>
-//       );
+import './Header.css';
+
+const Header = () => {
+    console.log('Header');
+
+    return (
+    <header className="header">
+        <div className='header__logo'>
+        <div>
+          <img src='/images/logo.svg' alt='logo'/>
+        </div>
+        <div>Date</div>
+        </div>
+        <div className='header__avatar-logo'>
+        <div>
+          <button type="text">Add New Clothes</button> 
+        </div>
+        <div>Name</div>
+        <div>
+        <img src='/images/Avatar.svg' alt='Avatar'/>
+        </div>
+        </div>
+      </header>
+    )
+}
+
+export default Header;
